1a43109931eba945704fa769a4fba5b198e05b97
afroraydude <thomasray0@gmail.com>
2017-03-25 21:40:16 -0400

    Trying to get all of this to work on my Mac

---
+++ Ro/Modules/Public/PublicModule.cs
@@ -0,0 +1,306 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using Imgur;
+using Imgur.API.Authentication;
+namespace DiscordExampleBot.Modules.Public
+{
+    public class PublicModule : ModuleBase
+    {
+        //  public string data;
+        public ModuleInfo _mi;
+        public DiscordSocketClient _cl;
+        public PublicModule(CommandService serv, DiscordSocketClient client)
+        {
+            ModuleInfo mi = serv.Modules.First();
+            DiscordSocketClient cl = client;
+            _mi = mi;
+            _cl = cl;
+        }
+
+        private string RetrieveGuildData(ulong guildID, DataType data)
+        {
+            DirectoryInfo dir;
+            FileStream fi;
+            if (Directory.Exists($"C:\\ArissBot\\Data\\{guildID}"))
+            {
+                dir = new DirectoryInfo($"C:\\ArissBot\\Data\\{guildID}");
+            }
+            else
+            {
+                dir = Directory.CreateDirectory($"C:\\ArissBot\\Data\\{guildID}");
+                Console.WriteLine($"Creating Directory {guildID} ({_cl.GetGuild(guildID).Name})...");
+            }
+            fi = new FileInfo($"{dir.FullName}\\{data}").Open(FileMode.OpenOrCreate);
+            Console.WriteLine($"Opening File {data}...");
+            StreamReader sr = new StreamReader(fi);
+            string tempstring = sr.ReadToEnd();
+            sr.Dispose();
+            return tempstring;
+        }
+        private void SetGuildData(ulong guildID, DataType data, string whatToWrite)
+        {
+            DirectoryInfo dir;
+            FileStream fi;
+            if (Directory.Exists($"C:\\ArissBot\\Data\\{guildID}"))
+            {
+                dir = new DirectoryInfo($"C:\\ArissBot\\Data\\{guildID}");
+            }
+            else
+            {
+                dir = Directory.CreateDirectory($"C:\\ArissBot\\Data\\{guildID}");
+                Console.WriteLine($"Creating Directory {guildID} ({_cl.GetGuild(guildID).Name})...");
+            }
+            fi = new FileInfo($"{dir.FullName}\\{data}").Open(FileMode.Append);
+            Console.WriteLine($"Writing to File {data}...");
+            StreamWriter sw = new StreamWriter(fi);
+            sw.Write( /** RetrieveGuildData(guildID, data) + **/ whatToWrite);
+            sw.Flush();
+            sw.Dispose();
+            return;
+        }
+        static Dictionary<string, string> tags = new Dictionary<string, string>();
+        [Command("invite")]
+        [Summary("Returns the OAuth2 Invite URL of the bot")]
+        public async Task Invite()
+        {
+            Console.WriteLine("Creating Invite...");
+            var application = await Context.Client.GetApplicationInfoAsync();
+            await ReplyAsync(
+                $"A user with `MANAGE_SERVER` can invite me to your server here: <https://discordapp.com/oauth2/authorize?client_id={application.Id}&scope=bot>");
+        }
+        [Command("hello")]
+        [Summary("Says hello!")]
+        public async Task Hello()
+        {
+            Console.WriteLine("Saying Hello...");
+            var name = Context.User.Username;
+            await ReplyAsync(
+                $"Hello {name}!");
+        }
+        [Command("pinfo")]
+        [Summary("Gets info about mentioned player.")]
+        public async Task GetInfo([Remainder] string mention = "0")
+        {
+            if (mention == "0")
+            {
+                mention = MentionUtils.MentionUser(Context.User.Id);
+            }
+            IGuildUser gusr = await Context.Guild.GetUserAsync(MentionUtils.ParseUser(mention));
+            string userdata =
+                $"{Format.Bold($"@{gusr.Username + "#" + gusr.Discriminator}")}\r" +
+                $"Joined server at {gusr.JoinedAt}.\r" +
+                $"Joined discord at {gusr.CreatedAt}.\r" +
+                $"Roles: ";
+            foreach (ulong id in gusr.RoleIds) {
+                userdata += Context.Guild.GetRole(id).Name + ", ";
+            }
+            await Context.Channel.SendMessageAsync($"", false, new EmbedBuilder()
+                .WithColor(Context.Guild.GetRole(gusr.RoleIds.Last()).Color)
+                .WithDescription(userdata).WithTitle($"{gusr.Nickname}")
+                .WithImageUrl($"{gusr.GetAvatarUrl(ImageFormat.Png, 128)}")
+                .Build());
+        }
+        [Command("clear")]
+        [Summary("Clears messages.")]
+        [RequireUserPermission(GuildPermission.ManageMessages)]
+        public async Task ClearMessages([Remainder] int b)
+        {
+            await Context.Channel.DeleteMessagesAsync(await Context.Channel.GetMessagesAsync(b + 1).Flatten());
+            await ReplyAsync($"{Context.Message.Author.Mention}, successfully cleared {b} messages!");
+        }
+        [Command("tag")]
+        [Summary("Returns the chosen tag.")]
+        public async Task ReadTag([Remainder] string input)
+        {
+            Dictionary<string, string> tagdict = new Dictionary<string, string>();
+            Console.WriteLine("Retrieving Tag...");
+            /** if (tags.TryGetValue(input, out string tagreturn) == true)
+            {
+                await ReplyAsync($"{tagreturn}");
+            }
+            else
+            {
+                await ReplyAsync($"Tag not found!");
+            }**/
+            string vars = RetrieveGuildData(Context.Guild.Id, DataType.Tags);
+            string[] varsplit = vars.Split('↓');
+            foreach (string str in varsplit)
+            {
+                string[] strsplit = str.Split('→');
+                tagdict.Add(strsplit.First(), strsplit.Last());
+            }
+            await ReplyAsync(tagdict[input]);
+        }
+        [Command("help")]
+        [Summary("Shows available commands.")]
+        public async Task Help()
+        {
+            var __mi = _mi.Commands.OrderBy(x => { return x.Aliases.First().ToCharArray().First(); }).ToArray();
+            string milist = string.Empty;
+            __mi.All(x => { milist += ($"{x.Aliases.First()}, {x.Summary} \n"); return true; });
+            Console.WriteLine("Listing Commands...");
+            EmbedBuilder emb = new EmbedBuilder();
+            emb.WithTitle(Format.Bold("Commands"))
+                .WithDescription($"{milist}")
+                .WithColor(new Color(200, 200, 200));
+            await ReplyAsync($"", false, emb.Build());
+        }
+        [Command("tags")]
+        [Summary("Lists the tags for this server")]
+        public async Task ListTags()
+        {
+            Dictionary<string, string> tagdict = new Dictionary<string, string>();
+            string vars = RetrieveGuildData(Context.Guild.Id, DataType.Tags);
+            string[] varsplit = vars.Split('↓');
+            foreach (string str in varsplit)
+            {
+                string[] strsplit = str.Split('→');
+                tagdict.Add(strsplit.First(), strsplit.Last());
+            }
+            await ReplyAsync(string.Join(", ", tagdict.Keys));
+        }
+        [Command("Emojify", RunMode = RunMode.Async)]
+        [Summary("Emojify's a message")]
+        public async Task Emojify([Remainder] string rem)
+        {
+            rem = rem.ToLower();
+            string estring = string.Empty;
+            foreach (char character in rem)
+            {
+                estring += $"{Em.GetEmoji(character.ToString())} ";
+            }
+            await ReplyAsync(estring);
+        }
+        [Command("GetFile")]
+        [Summary("Gets a retrieves a file from bot's host")]
+        [RequireOwner]
+        public async Task GetFile([Remainder] string filename)
+        {
+            Console.WriteLine("Getting File...");
+            await Context.Channel.SendFileAsync(filename);
+        }
+        [Command("dir")]
+        [Summary("returns files in directory from host")]
+        [RequireOwner]
+        public async Task GetDir([Remainder] string filename)
+        {
+            Console.WriteLine("Getting File...");
+            string dirs = "";
+            foreach (FileSystemInfo fs in new DirectoryInfo(filename).GetFileSystemInfos())
+            {
+                if (fs.Extension == string.Empty)
+                {
+                    dirs += $"<dir> {fs.Name},\r";
+                }
+                else
+                {
+                    dirs += $"{fs.Name},\r";
+                }
+            }
+            await ReplyAsync($"{Format.Code(dirs, "bat")}");
+        }
+        [Command("create")]
+        [Summary("Creates a tag.")]
+        public async Task WriteTag([Remainder] string input)
+        {
+            Console.WriteLine("Creating Tag...");
+            string[] _args = input.Split(' ');
+            List<string> inputlist = _args.ToList();
+            SetGuildData(Context.Guild.Id, DataType.Tags, inputlist[0] + "→" + $"{ Format.Code(String.Join(" ", inputlist.Skip(1).ToArray()))}" + "↓");
+            await ReplyAsync($"Added tag {inputlist[0]}!");
+        }
+        [Command("set")]
+        [RequireUserPermission(GuildPermission.Administrator)]
+        public async Task Set([Remainder]string remaining)
+        {
+            string targetvar = remaining.Split(' ').First();
+            string value = remaining.Split(' ').Last();
+            if (targetvar == "linkFilter" && value == "1")
+            {
+                Console.WriteLine("Assigning to delegate...");
+                _cl.MessageReceived += (async (x) => {
+                    bool hasLink = DetectLinks(x);
+                    if (hasLink)
+                    {
+                        await x.DeleteAsync();
+                        IMessage im = await x.Channel.SendMessageAsync($"{Format.Bold("Only one link per message 😡")}");
+                        await DeleteMsg(im, 1500);
+                        return;
+                    }
+                    else return;
+                });
+                bool DetectLinks(SocketMessage sm)
+                {
+                    Regex reg = new Regex("http", RegexOptions.IgnoreCase);
+                    MatchCollection matches = reg.Matches(sm.Content);
+                    if (matches.Count > 1)
+                    {
+                        return true;
+                    }
+                    else return false;
+                }
+                async Task DeleteMsg(IMessage msg, int delayInMilliseconds)
+                {
+                    await Task.Delay(delayInMilliseconds).ContinueWith(async (x) =>
+                    {
+                        await msg.DeleteAsync();
+                    });
+                }
+                await ReplyAsync($"Updated tag {targetvar}!");
+            }
+        }
+        [Command("say")]
+        [Alias("echo")]
+        [RequireOwner]
+        [Summary("Echos the provided input.")]
+        public async Task Say([Remainder] string input)
+        {
+            Console.WriteLine("Mocking...");
+            await ReplyAsync(input);
+        }
+        [Command("Support")]
+        [Summary("Returns a link to Ro's support server")]
+        public async Task GetServer()
+        {
+            await ReplyAsync($"Do you need help with Ro? Visit the support server: https://discord.gg/4QcF7fx");
+        }
+        [Command("info")]
+        [Summary("Detailed info about the bot.")]
+        public async Task Info()
+        {
+            Console.WriteLine("Providing Information...");
+            var application = await Context.Client.GetApplicationInfoAsync();
+            await ReplyAsync(
+                $"{Format.Bold("Info")}\n" +
+                $"- Author: {application.Owner.Username} (ID {application.Owner.Id})\n" +
+                $"- Library: Discord.Net ({DiscordConfig.Version})\n" +
+                $"- Runtime: {RuntimeInformation.FrameworkDescription} {RuntimeInformation.OSArchitecture}\n" +
+                $"- Uptime: {GetUptime()}\n" +
+                $"- Ping: {(Context.Client as DiscordSocketClient).Latency}ms\n\n" +
+                $"{Format.Bold("Stats")}\n" +
+                $"- Heap Size: {GetHeapSize()} MB\n" +
+                $"- Guilds: {(Context.Client as DiscordSocketClient).Guilds.Count}\n" +
+                $"- Channels: {(Context.Client as DiscordSocketClient).Guilds.Sum(g => g.Channels.Count)}" +
+                $"- Users: {(Context.Client as DiscordSocketClient).Guilds.Sum(g => g.Users.Count)}"
+            );
+        }
+        [Command("embed")]
+        [Summary("creates embed with title, desc, footer, separate with $")]
+        public async Task CreateEmbed([Remainder] string rem) {
+            string[] splitrem = rem.Split('$');
+            await ReplyAsync("", false, new EmbedBuilder().WithTitle(splitrem[0]).WithDescription(splitrem[splitrem.Length-2]).WithFooter(new EmbedFooterBuilder().WithText(splitrem.Last())).Build());
+        }
+        private static string GetUptime()
+            => (DateTime.Now - Process.GetCurrentProcess().StartTime).ToString(@"dd\.hh\:mm\:ss");
+        private static string GetHeapSize() => Math.Round(GC.GetTotalMemory(true) / (1024.0 * 1024.0), 2).ToString();
+    }
+}
---
+++ Ro/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.IO;
+using Discord;
+using Discord.WebSocket;
+using Discord.Commands;
+
+namespace DiscordExampleBot
+{
+    enum DataType
+    {
+        Vars = 'v',
+        Levels = 'l',
+        Tags = 't',
+    }
+    static class Em
+    {
+        public static string GetEmoji(string val)
+        {
+            if (val == "a") return a;
+            if (val == "b") return b;
+            if (val == "c") return c;
+            if (val == "d") return d;
+            if (val == "e") return e;
+            if (val == "f") return f;
+            if (val == "g") return g;
+            if (val == "h") return h;
+            if (val == "i") return i;
+            if (val == "j") return j;
+            if (val == "k") return k;
+            if (val == "l") return l;
+            if (val == "m") return m;
+            if (val == "n") return n;
+            if (val == "o") return o;
+            if (val == "p") return p;
+            if (val == "q") return q;
+            if (val == "r") return r;
+            if (val == "s") return s;
+            if (val == "t") return t;
+            if (val == "u") return u;
+            if (val == "v") return v;
+            if (val == "w") return w;
+            if (val == "x") return x;
+            if (val == "y") return y;
+            if (val == "z") return z;
+            else return "";
+        }
+        public static string a = "🇦";
+        public static string b = "🇧";
+        public static string c = "🇨";
+        public static string d = "🇩";
+        public static string e = "🇪";
+        public static string f = "🇫";
+        public static string g = "🇬";
+        public static string h = "🇭";
+        public static string i = "🇮";
+        public static string j = "🇯";
+        public static string k = "🇰";
+        public static string l = "🇱";
+        public static string m = "🇲";
+        public static string n = "🇳";
+        public static string o = "🇴";
+        public static string p = "🇵";
+        public static string q = "🇶";
+        public static string r = "🇷";
+        public static string s = "🇸";
+        public static string t = "🇹";
+        public static string u = "🇺";
+        public static string v = "🇻";
+        public static string w = "🇼";
+        public static string x = "🇽";
+        public static string y = "🇾";
+        public static string z = "🇿";
+    }
+    public class Program
+    {
+        // string data;
+        // Convert our sync main to an async main.
+        public static void Main(string[] args) =>
+            new Program().Start().GetAwaiter().GetResult();
+        private DiscordSocketClient client;
+        private CommandHandler handler;
+
+        public async Task Start()
+        {
+            // Define the DiscordSocketClient
+            client = new DiscordSocketClient();
+            client.Log += (async x => {
+                await Console.Error.WriteLineAsync(x.Message);
+            });
+            var token = "insert token here";
+
+            // Login and connect to Discord.
+            Console.WriteLine("Logging in...");
+            await client.LoginAsync(TokenType.Bot, token).ContinueWith(x =>
+           {
+               client.SetStatusAsync(UserStatus.Online);
+               client.SetGameAsync("Type >help!", "", StreamType.Twitch);
+           });
+            await client.StartAsync();
+            
+            var map = new DependencyMap();
+            Console.WriteLine("Adding Client to map...");
+            map.Add(client);
+
+            handler = new CommandHandler();
+            Console.WriteLine("Installing map...");
+            await handler.Install(map);
+
+            // Block this program until it is closed.
+            client.JoinedGuild += (async x => {
+                await x.DefaultChannel.SendMessageAsync($"Hello I am Ro 😄.\rI am currently in early development so please report any problems to @Ariss#4202.\rTHANKS and have fun with me :).");
+                Console.WriteLine($"I have joined the server {x.Name} :P");
+                return;
+            });
+            Console.WriteLine("Ready!");
+            await GetInput();
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+        async private Task GetInput()
+        {
+            int a = 1;
+            while (a == 1)
+            {
+                string input = Console.ReadLine();
+                if (input == "stop")
+                {
+                    Console.Write("Setting Status to offline...");
+                    await client.SetStatusAsync(UserStatus.Invisible);
+                    Console.Write("Stopping...");
+                    await client.StopAsync();
+                    Console.WriteLine("Logging Out...");
+                    await client.LogoutAsync();
+                    Console.WriteLine("The bot has been shut down, until next time ;)");
+                }
+            }
+            return;
+        }
+    }
+}
